0e81c22a7b047a7239079b27a1c1b4eb5afc4900
vikrantaroraa <arora.veshu@gmail.com>
2024-11-21 23:28:14 +0530

    feat: added the most basic popup to the extension

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,13 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id,
-    },
-    func: () => {
-      alert("Hello from my extension!");
-    },
-  });
-});
+// We don't actually need this code, it was just to understand how to run a script in the background on clicking
+// the extension icon.
+
+// chrome.action.onClicked.addListener((tab) => {
+//   chrome.scripting.executeScript({
+//     target: {
+//       tabId: tab.id,
+//     },
+//     func: () => {
+//       alert("Hello from my extension!");
+//     },
+//   });
+// });
